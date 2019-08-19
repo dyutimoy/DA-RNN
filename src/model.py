39327174d779b8b7384e06a1142b564fe943dbe5
xenocrypt <dakash1897@gmail.com>
2019-08-19 12:49:57 +0530

    32 layer with increased iter

--- src/model.py
+++ src/model.py
@@ -432,13 +432,13 @@ class DA_rnn(nn.Module):
         
         if(self.resume==True):
                 
-                checkpointencoder = torch.load("../weights_new/EncoderEpochnew"+"50"+"_17_8.pt")
+                checkpointencoder = torch.load("../weights_new/EncoderEpochnew"+"250"+"_17_8.pt")
                 self.Encoder.load_state_dict(checkpointencoder['model_state_dict'])
                 self.encoder_optimizer.load_state_dict(checkpointencoder['optimizer_state_dict'])
                 epoch_start = checkpointencoder['epoch']
                 loss = checkpointencoder['loss']
 
-                checkpointdecoder = torch.load("../weights_new/DecoderEpochnew"+"50"+"_17_8.pt")
+                checkpointdecoder = torch.load("../weights_new/DecoderEpochnew"+"250"+"_17_8.pt")
                 self.Decoder.load_state_dict(checkpointdecoder['model_state_dict'])
                 self.decoder_optimizer.load_state_dict(checkpointdecoder['optimizer_state_dict'])
                 print("loading checkpoint "+str(epoch_start)+"with loss"+str(loss))
@@ -526,7 +526,7 @@ class DA_rnn(nn.Module):
 
             if epoch % 2 == 0:
                 print ("Epochs: ", epoch+epoch_start, " Iterations: ", n_iter, " Loss: ", self.epoch_losses[epoch])
-            if epoch % 50 == 0 and self.epoch_losses[epoch]<min_loss:
+            if epoch % 25 == 0 and self.epoch_losses[epoch]<min_loss:
                 print("Model's state_dict:")
                 for param_tensor in self.Encoder.state_dict():
                     print(param_tensor, "\t", self.Encoder.state_dict()[param_tensor].size())
